cf60841d2858e5d3f93e472af237f559106b223d
22nancy-pixel <nancyelorm8@gmail.com>
2025-02-06 14:16:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,3 +16,9 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/your-db-name', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
